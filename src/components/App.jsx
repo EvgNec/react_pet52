fdbc5fd0386a6add9295c53f12ddcbcd546e5a56
Evgeniy <evgnec@gmail.com>
2025-07-19 16:26:24 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+// import Home from "path/to/pages/Home";
+// import About from "path/to/pages/About";
+// import Products from "path/to/pages/Products";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    <nav>
+      <NavLink to='/'>HomePage</NavLink>
+      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/products'>Products</NavLink>
+    </nav>
+      <Routes>
+      <Route path="/" element={<div>HP</div>}/>
+      <Route path="/about" element={<div>about</div>}/>
+      <Route path="/products" element={<div>products</div>}/>
+
+      </Routes>
     </div>
   );
 };
